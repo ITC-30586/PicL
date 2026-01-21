131707a389583083ea3d1880033bb9061b7b422f
林　光邦 <mitsukuni.hayashi@mail.toyota-shokki.co.jp>
2026-01-21 09:32:37 +0900

    20260121

--- PicL/Form1.Designer.vb
+++ PicL/Form1.Designer.vb
@@ -22,35 +22,81 @@ Partial Class Form1
     'コード エディターを使って変更しないでください。
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.C1FlexGrid1 = New C1.Win.C1FlexGrid.C1FlexGrid()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.C1FlexGrid1 = New C1.Win.C1FlexGrid.C1FlexGrid()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
         CType(Me.C1FlexGrid1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Location = New System.Drawing.Point(0, 0)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(1906, 993)
+        Me.TabControl1.TabIndex = 4
+        '
+        'TabPage1
+        '
+        Me.TabPage1.BackColor = System.Drawing.Color.DarkGray
+        Me.TabPage1.Controls.Add(Me.TextBox2)
+        Me.TabPage1.Controls.Add(Me.ComboBox1)
+        Me.TabPage1.Controls.Add(Me.TextBox1)
+        Me.TabPage1.Controls.Add(Me.Button1)
+        Me.TabPage1.Controls.Add(Me.C1FlexGrid1)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(1898, 967)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "作業"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.Font = New System.Drawing.Font("MS UI Gothic", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(1579, 293)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(200, 23)
+        Me.ComboBox1.TabIndex = 5
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(1619, 458)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 19)
+        Me.TextBox1.TabIndex = 4
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(1579, 394)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
         '
         'C1FlexGrid1
         '
         Me.C1FlexGrid1.ColumnInfo = "10,1,0,0,0,-1,Columns:"
-        Me.C1FlexGrid1.Location = New System.Drawing.Point(29, 24)
+        Me.C1FlexGrid1.Location = New System.Drawing.Point(8, 6)
         Me.C1FlexGrid1.Name = "C1FlexGrid1"
         Me.C1FlexGrid1.Size = New System.Drawing.Size(1504, 906)
-        Me.C1FlexGrid1.TabIndex = 1
+        Me.C1FlexGrid1.TabIndex = 2
         '
-        'Button1
+        'TextBox2
         '
-        Me.Button1.Location = New System.Drawing.Point(1751, 328)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 2
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
-        '
-        'TextBox1
-        '
-        Me.TextBox1.Location = New System.Drawing.Point(1740, 568)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(100, 19)
-        Me.TextBox1.TabIndex = 3
+        Me.TextBox2.Location = New System.Drawing.Point(1579, 146)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(191, 19)
+        Me.TextBox2.TabIndex = 6
         '
         'Form1
         '
@@ -58,17 +104,21 @@ Partial Class Form1
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.DarkGray
         Me.ClientSize = New System.Drawing.Size(1904, 991)
-        Me.Controls.Add(Me.TextBox1)
-        Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.C1FlexGrid1)
+        Me.Controls.Add(Me.TabControl1)
         Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.TabPage1.PerformLayout()
         CType(Me.C1FlexGrid1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
-    Friend WithEvents C1FlexGrid1 As C1.Win.C1FlexGrid.C1FlexGrid
-    Friend WithEvents Button1 As Button
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents TabPage1 As TabPage
     Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents C1FlexGrid1 As C1.Win.C1FlexGrid.C1FlexGrid
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents TextBox2 As TextBox
 End Class
--- PicL/Form1.vb
+++ PicL/Form1.vb
@@ -8,6 +8,8 @@ Imports C1.Framework.Extension
 Imports C1.Util.Win.Win32
 Imports C1.Win.C1FlexGrid
 Imports C1.Win.TouchToolKit
+Imports Npgsql
+
 
 Public Class Form1
 
@@ -16,6 +18,24 @@ Public Class Form1
     Private Const CELL_HEIGHT As Integer = 9
     Private Const CELL_WIDTH As Integer = 15
     Private Const APP_VERSION As String = "Ver1.0.0"
+    Private Const FONT_SIZE As Single = 6
+    Private Const FONT_NAME As String = "ＭＳ 明朝"
+
+    ' ComboBoxインデックス定数
+    Private Const COMBO_INDEX_NONE As Integer = 0
+    Private Const COMBO_INDEX_CLEAR As Integer = 1
+    Private Const COMBO_INDEX_STYLE1 As Integer = 2
+    Private Const COMBO_INDEX_STYLE2 As Integer = 3
+    Private Const COMBO_INDEX_STYLE3 As Integer = 4
+    Private Const COMBO_INDEX_STYLE4 As Integer = 5
+    Private Const COMBO_INDEX_STYLE5 As Integer = 6
+
+    ' セルスタイル名定数
+    Private Const STYLE_NS1 As String = "NS1"
+    Private Const STYLE_NS2 As String = "NS2"
+    Private Const STYLE_NS3 As String = "NS3"
+    Private Const STYLE_NS4 As String = "NS4"
+    Private Const STYLE_NS5 As String = "NS5"
 
     ' フィールド
     Private stCurrentDir As String
@@ -23,11 +43,22 @@ Public Class Form1
     Private styleNS2 As CellStyle
     Private styleNS3 As CellStyle
     Private styleNS4 As CellStyle
+    Private styleNS5 As CellStyle
+
+    ' データベース接続情報
+    Private dbIPAddress As String
+    Private dbPort As String
+    Private dbName As String
+    Private selectedComboIndex As Integer = COMBO_INDEX_NONE
+
+    Private ReadOnly cn1 As New NpgsqlConnection
+    Private ReadOnly cn2 As New NpgsqlConnection
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
         Try
             ' 2重起動防止
             If IsApplicationAlreadyRunning() Then
+                MessageBox.Show("アプリケーションは既に起動しています。", "警告", MessageBoxButtons.OK, MessageBoxIcon.Warning)
                 Me.Close()
                 Return
             End If
@@ -36,182 +67,339 @@ Public Class Form1
             stCurrentDir = System.IO.Directory.GetCurrentDirectory()
 
             ' 初期化処理
-            'InitializeZoomPanel()
-            Call Init_Form1()
-            Call Init_C1FG01()
+            Call LoadConfiguration()
+            Call ConnectToDatabase()
+            Call InitializeForm()
+            Call InitializeGrid()
             Call InitializeCellStyles()
 
+        Catch ex As IO.FileNotFoundException
+            WriteErrorLog("Form1_Load", ex.Message)
+            MessageBox.Show($"必要なファイルが見つかりません: {ex.FileName}", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Me.Close()
+        Catch ex As NpgsqlException
+            WriteErrorLog("Form1_Load", ex.Message)
+            MessageBox.Show("データベース接続エラーが発生しました。設定を確認してください。", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Me.Close()
         Catch ex As Exception
-            ErrLOG("Form1_Load", ex.Message)
+            WriteErrorLog("Form1_Load", ex.Message)
             MessageBox.Show("初期化中にエラーが発生しました。", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Me.Close()
         End Try
     End Sub
 
     ' 2重起動チェック
     Private Function IsApplicationAlreadyRunning() As Boolean
-        Dim pn As String = Process.GetCurrentProcess.ProcessName
-        Return Process.GetProcessesByName(pn).GetUpperBound(0) > 0
+        Dim processName As String = Process.GetCurrentProcess.ProcessName
+        Return Process.GetProcessesByName(processName).Length > 1
     End Function
 
-    '' ZoomPanelの初期化
-    'Private Sub InitializeZoomPanel()
-    '    With C1ZoomPanel1
-    '        .ZoomFactor = 1.0F
-    '        .MaxZoomFactor = 4.0F
-    '        .AllowDoubleTapZoom = True
-    '        .ZoomSnapDistance = 0.05F
-    '        .ZoomSnapPoints.Add(2.0F)
-    '        .ZoomSnapPoints.Add(3.0F)
-    '        .ZoomSnapPoints.Add(4.0F)
-    '        .InnerPanelLayoutMode = InnerPanelLayoutMode.MiddleCenter
-    '        .BoundaryFeedbackMode = BoundaryFeedbackMode.Standard
-    '    End With
-    'End Sub
+    ' 設定ファイル読み込み (旧:Initialization)
+    Private Sub LoadConfiguration()
+        Try
+            Dim configPath As String = System.IO.Path.Combine(My.Application.Info.DirectoryPath, "INI.xml")
+
+            If Not System.IO.File.Exists(configPath) Then
+                Throw New IO.FileNotFoundException("設定ファイルが見つかりません。", configPath)
+            End If
+
+            ' XMLファイルの読み込み
+            Dim xmlDoc As New System.Xml.XmlDocument
+            xmlDoc.Load(configPath)
+            Dim rootElement As System.Xml.XmlElement = xmlDoc.DocumentElement
+
+            If rootElement Is Nothing Then
+                Throw New System.Xml.XmlException("XMLファイルのルート要素が見つかりません。")
+            End If
+
+            ' DB設定取得
+            dbIPAddress = GetXmlNodeValue(rootElement, "db_ip", "133.222.186.64")
+            dbPort = GetXmlNodeValue(rootElement, "db_port", "25432")
+            dbName = GetXmlNodeValue(rootElement, "db_name", "dbpiclocation")
+
+        Catch ex As Exception
+            WriteErrorLog("LoadConfiguration", ex.Message)
+            Throw
+        End Try
+    End Sub
+
+    ' XMLノード値取得ヘルパー
+    Private Function GetXmlNodeValue(rootElement As System.Xml.XmlElement, tagName As String, defaultValue As String) As String
+        Dim nodes As System.Xml.XmlNodeList = rootElement.GetElementsByTagName(tagName)
+        If nodes.Count > 0 AndAlso Not String.IsNullOrWhiteSpace(nodes.Item(0).InnerText) Then
+            Return nodes.Item(0).InnerText.Trim()
+        End If
+        Return defaultValue
+    End Function
+
+    ' データベース接続 (旧:DB_Connect)
+    Private Sub ConnectToDatabase()
+        Try
+            Dim connectionString As String = BuildConnectionString()
+            cn1.ConnectionString = connectionString
+            cn2.ConnectionString = connectionString
+
+            ' 接続テスト
+            cn1.Open()
+            cn1.Close()
+
+        Catch ex As NpgsqlException
+            WriteErrorLog("ConnectToDatabase", ex.Message)
+            Throw
+        Catch ex As Exception
+            WriteErrorLog("ConnectToDatabase", ex.Message)
+            Throw
+        End Try
+    End Sub
+
+    ' 接続文字列構築
+    Private Function BuildConnectionString() As String
+        Return $"Server={dbIPAddress};Port={dbPort};User id=postgres;Password=postgres;Database={dbName};"
+    End Function
 
     ' セルスタイルの初期化
     Private Sub InitializeCellStyles()
         ' 作業A用スタイル
-        styleNS1 = C1FlexGrid1.Styles.Add("NS1")
-        With styleNS1
-            .BackColor = System.Drawing.Color.FromArgb(50, 169, 169, 169)
-            .ForeColor = System.Drawing.Color.Crimson
-            .Font = New Font("ＭＳ 明朝", 6, FontStyle.Regular)
-            .TextAlign = TextAlignEnum.CenterCenter
-        End With
+        styleNS1 = CreateCellStyle(STYLE_NS1, System.Drawing.Color.FromArgb(50, 169, 169, 169), System.Drawing.Color.Crimson)
 
         ' 作業B用スタイル
-        styleNS2 = C1FlexGrid1.Styles.Add("NS2")
-        With styleNS2
-            .BackColor = System.Drawing.Color.FromArgb(50, 210, 180, 140)
-            .ForeColor = System.Drawing.Color.DodgerBlue
-            .Font = New Font("ＭＳ 明朝", 6, FontStyle.Regular)
-            .TextAlign = TextAlignEnum.CenterCenter
-        End With
+        styleNS2 = CreateCellStyle(STYLE_NS2, System.Drawing.Color.FromArgb(50, 210, 180, 140), System.Drawing.Color.DodgerBlue)
 
         ' 作業C用スタイル
-        styleNS3 = C1FlexGrid1.Styles.Add("NS3")
-        With styleNS3
-            .BackColor = System.Drawing.Color.FromArgb(50, 135, 206, 235)
-            .ForeColor = System.Drawing.Color.Green
-            .Font = New Font("ＭＳ 明朝", 6, FontStyle.Regular)
-            .TextAlign = TextAlignEnum.CenterCenter
-        End With
+        styleNS3 = CreateCellStyle(STYLE_NS3, System.Drawing.Color.FromArgb(50, 135, 206, 235), System.Drawing.Color.Green)
 
-        ' 作業混合用スタイル
-        styleNS4 = C1FlexGrid1.Styles.Add("NS4")
-        With styleNS4
-            .BackColor = System.Drawing.Color.FromArgb(50, 255, 127, 80)
-            .ForeColor = System.Drawing.Color.SaddleBrown
-            .Font = New Font("ＭＳ 明朝", 6, FontStyle.Regular)
-            .TextAlign = TextAlignEnum.CenterCenter
-        End With
+        ' 作業D用スタイル
+        styleNS4 = CreateCellStyle(STYLE_NS4, System.Drawing.Color.FromArgb(50, 255, 127, 80), System.Drawing.Color.SaddleBrown)
+
+        ' 作業E用スタイル
+        styleNS5 = CreateCellStyle(STYLE_NS5, System.Drawing.Color.FromArgb(50, 255, 215, 0), System.Drawing.Color.DarkOliveGreen)
     End Sub
 
-    ' エラーログを書き出す
-    Public Sub ErrLOG(ByVal C1 As String, ByVal C2 As String)
-        Dim FileName As String
-        Dim WS As System.IO.StreamWriter
+    ' セルスタイル作成ヘルパー
+    Private Function CreateCellStyle(styleName As String, backColor As System.Drawing.Color, foreColor As System.Drawing.Color) As CellStyle
+        Dim style As CellStyle = C1FlexGrid1.Styles.Add(styleName)
+        With style
+            .BackColor = backColor
+            .ForeColor = foreColor
+            .Font = New Font(FONT_NAME, FONT_SIZE, FontStyle.Regular)
+            .TextAlign = TextAlignEnum.CenterCenter
+        End With
+        Return style
+    End Function
 
+    ' エラーログを書き出す
+    Private Sub WriteErrorLog(methodName As String, errorMessage As String)
         Try
-            FileName = stCurrentDir & "\errlog.txt"
-            WS = System.IO.File.AppendText(FileName)
-            WS.WriteLine($"{C1}:{C2}:{Format(Now, "yyyy/MM/dd HH:mm:ss")}")
-            WS.Close()
+            Dim logFilePath As String = System.IO.Path.Combine(stCurrentDir, "errlog.txt")
+            Dim timestamp As String = DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss")
+            Dim logEntry As String = $"[{timestamp}] {methodName}: {errorMessage}{Environment.NewLine}"
+
+            System.IO.File.AppendAllText(logFilePath, logEntry, System.Text.Encoding.UTF8)
         Catch ex As Exception
             Debug.WriteLine($"エラーログ書き込み失敗: {ex.Message}")
         End Try
     End Sub
 
     ' 起動時画面設定
-    Private Sub Init_Form1()
+    Private Sub InitializeForm()
         Me.Text = $"PICロケーション作業記録 {APP_VERSION}"
+
+        TabControl1.SizeMode = TabSizeMode.Fixed
+        TabControl1.ItemSize = New Drawing.Size(100, 30)
+
+        InitializeComboBox()
     End Sub
 
-    Private Sub Init_C1FG01()
+    ' グリッド初期化 (旧:Init_C1FG01)
+    Private Sub InitializeGrid()
         Try
             C1FlexGrid1.Visible = False
+            C1FlexGrid1.Location = New Drawing.Point(29, 24)
+            C1FlexGrid1.Size = New Drawing.Size(1504, 906)
             C1FlexGrid1.Clear()
 
             ' 背景画像設定
-            Dim imagePath As String = System.Windows.Forms.Application.StartupPath & "\Sample.png"
-            If System.IO.File.Exists(imagePath) Then
-                C1FlexGrid1.BackgroundImage = Image.FromFile(imagePath)
-                C1FlexGrid1.BackgroundImageLayout = ImageLayout.Stretch
-            End If
-
-            '(1,1)-(100,100)のグリッド作成
-            C1FlexGrid1.Cols(0).Visible = False
-            C1FlexGrid1.Rows(0).Visible = False
-
-            ' 罫線設定
-            With C1FlexGrid1.Styles.Normal.Border
-                .Color = System.Drawing.Color.Gray
-                .Style = BorderStyleEnum.Dotted
-                .Width = 1
-            End With
-
-            ' フォーカス罫線設定
-            With C1FlexGrid1.Styles.Focus.Border
-                .Color = System.Drawing.Color.Red
-                .Style = BorderStyleEnum.Double
-            End With
+            LoadBackgroundImage()
 
             ' グリッド設定
-            With C1FlexGrid1
-                .Rows.DefaultSize = CELL_HEIGHT
-                .Cols.DefaultSize = CELL_WIDTH
-                .Rows.Fixed = 0
-                .Cols.Fixed = 0
-                .Cols.Count = GRID_SIZE + 1
-                .Rows.Count = GRID_SIZE + 1
-                .ScrollBars = ScrollBars.None
-
-                For i As Integer = 0 To GRID_SIZE
-                    .Cols(i).Width = CELL_WIDTH
-                    .Rows(i).Height = CELL_HEIGHT
-                Next
-            End With
+            ConfigureGridAppearance()
+            ConfigureGridSize()
 
             C1FlexGrid1.Visible = True
             C1FlexGrid1.Select(-1, -1, True)
 
         Catch ex As Exception
-            ErrLOG("Init_C1FG01", ex.Message)
+            WriteErrorLog("InitializeGrid", ex.Message)
             Throw
         End Try
     End Sub
 
-    ' レコード選択
+    ' 背景画像読み込み
+    Private Sub LoadBackgroundImage()
+        Dim imagePath As String = System.IO.Path.Combine(Application.StartupPath, "Sample.png")
+        If System.IO.File.Exists(imagePath) Then
+            C1FlexGrid1.BackgroundImage = Image.FromFile(imagePath)
+            C1FlexGrid1.BackgroundImageLayout = ImageLayout.Stretch
+        End If
+    End Sub
+
+    ' グリッド外観設定
+    Private Sub ConfigureGridAppearance()
+        C1FlexGrid1.Cols(0).Visible = False
+        C1FlexGrid1.Rows(0).Visible = False
+
+        ' 罫線設定
+        With C1FlexGrid1.Styles.Normal.Border
+            .Color = System.Drawing.Color.Gray
+            .Style = BorderStyleEnum.Dotted
+            .Width = 1
+        End With
+
+        ' フォーカス罫線設定
+        With C1FlexGrid1.Styles.Focus.Border
+            .Color = System.Drawing.Color.Red
+            .Style = BorderStyleEnum.Double
+        End With
+    End Sub
+
+    ' グリッドサイズ設定
+    Private Sub ConfigureGridSize()
+        With C1FlexGrid1
+            .Rows.DefaultSize = CELL_HEIGHT
+            .Cols.DefaultSize = CELL_WIDTH
+            .Rows.Fixed = 0
+            .Cols.Fixed = 0
+            .Cols.Count = GRID_SIZE + 1
+            .Rows.Count = GRID_SIZE + 1
+            .ScrollBars = ScrollBars.None
+
+            For i As Integer = 0 To GRID_SIZE
+                .Cols(i).Width = CELL_WIDTH
+                .Rows(i).Height = CELL_HEIGHT
+            Next
+        End With
+    End Sub
+
+    ' セル選択イベント
     Private Sub C1FlexGrid1_Click(sender As Object, e As EventArgs) Handles C1FlexGrid1.Click
         Try
-            Dim x As Integer = C1FlexGrid1.Col
-            Dim y As Integer = C1FlexGrid1.Row
+            Dim colIndex As Integer = C1FlexGrid1.Col
+            Dim rowIndex As Integer = C1FlexGrid1.Row
 
-            ' 無効な選択をチェック
-            If y <= 0 OrElse x < 0 Then
-                Exit Sub
+            ' ComboBoxが選択されていない場合は処理無効
+            If selectedComboIndex = COMBO_INDEX_NONE Then
+                Return
             End If
 
-            ' セルにデータを設定
-            C1FlexGrid1.SetCellStyle(y, x, styleNS4)
-            C1FlexGrid1.SetData(y, x, "●")
+            ' 無効な選択をチェック
+            If rowIndex <= 0 OrElse colIndex < 0 Then
+                Return
+            End If
+
+            ' ComboBoxの選択に応じてセルスタイルを変更
+            ApplyCellStyle(rowIndex, colIndex, selectedComboIndex)
 
             ' 作業位置を表示
-            TextBox1.Text = $"({x},{y})"
+            TextBox1.Text = $"({colIndex},{rowIndex})"
 
             ' カレントセルのカーソルを非表示
             C1FlexGrid1.Row = -1
             C1FlexGrid1.Col = -1
 
+            'DB保存処理をここに追加可能
+            Select Case selectedComboIndex
+                Case COMBO_INDEX_CLEAR
+                    ' 保存処理クリア
+
+
+
+
+
+                Case COMBO_INDEX_STYLE1
+                    ' 保存処理A
+                Case COMBO_INDEX_STYLE2
+                    ' 保存処理B
+                Case COMBO_INDEX_STYLE3
+                    ' 保存処理C
+                Case COMBO_INDEX_STYLE4
+                    ' 保存処理D
+                Case COMBO_INDEX_STYLE5
+                    ' 保存処理E
+            End Select
+
+
+
+
+
+
         Catch ex As Exception
-            ErrLOG("C1FlexGrid1_Click", ex.Message)
+            WriteErrorLog("C1FlexGrid1_Click", ex.Message)
             MessageBox.Show("セル選択中にエラーが発生しました。", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Warning)
         End Try
     End Sub
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs)
+    ' セルスタイル適用
+    Private Sub ApplyCellStyle(rowIndex As Integer, colIndex As Integer, comboIndex As Integer)
+        Select Case comboIndex
+            Case COMBO_INDEX_CLEAR
+                C1FlexGrid1.SetData(rowIndex, colIndex, "")
+            Case COMBO_INDEX_STYLE1
+                C1FlexGrid1.SetCellStyle(rowIndex, colIndex, styleNS1)
+                C1FlexGrid1.SetData(rowIndex, colIndex, "●")
+            Case COMBO_INDEX_STYLE2
+                C1FlexGrid1.SetCellStyle(rowIndex, colIndex, styleNS2)
+                C1FlexGrid1.SetData(rowIndex, colIndex, "◆")
+            Case COMBO_INDEX_STYLE3
+                C1FlexGrid1.SetCellStyle(rowIndex, colIndex, styleNS3)
+                C1FlexGrid1.SetData(rowIndex, colIndex, "■")
+            Case COMBO_INDEX_STYLE4
+                C1FlexGrid1.SetCellStyle(rowIndex, colIndex, styleNS4)
+                C1FlexGrid1.SetData(rowIndex, colIndex, "▲")
+            Case COMBO_INDEX_STYLE5
+                C1FlexGrid1.SetCellStyle(rowIndex, colIndex, styleNS5)
+                C1FlexGrid1.SetData(rowIndex, colIndex, "▼")
+        End Select
+    End Sub
 
+    ' ComboBox初期化 (旧:Init_Combo01)
+    Private Sub InitializeComboBox()
+        With ComboBox1
+            .Items.Clear()
+            .Items.AddRange(New String() {"", "Clear", "①", "②", "③", "④", "⑤"})
+            .SelectedIndex = 0
+            .DropDownStyle = ComboBoxStyle.DropDownList
+        End With
+    End Sub
 
+    ' ComboBox選択変更イベント
+    Private Sub ComboBox1_TextChanged(sender As Object, e As EventArgs) Handles ComboBox1.TextChanged
+        selectedComboIndex = ComboBox1.SelectedIndex
+        C1FlexGrid1.Select()
+    End Sub
+
+    ' フォームクローズ時のリソース解放
+    Protected Overrides Sub OnFormClosing(e As FormClosingEventArgs)
+        Try
+            CloseConnections()
+        Catch ex As Exception
+            WriteErrorLog("OnFormClosing", ex.Message)
+        Finally
+            MyBase.OnFormClosing(e)
+        End Try
+    End Sub
+
+    ' データベース接続のクローズ
+    Private Sub CloseConnections()
+        Try
+            If cn1 IsNot Nothing AndAlso cn1.State = ConnectionState.Open Then
+                cn1.Close()
+            End If
+            If cn2 IsNot Nothing AndAlso cn2.State = ConnectionState.Open Then
+                cn2.Close()
+            End If
+        Catch ex As Exception
+            WriteErrorLog("CloseConnections", ex.Message)
+        End Try
     End Sub
 
 End Class
